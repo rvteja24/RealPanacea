ff40bbf471980408b629991e9406c6d79955852b
VIJAYA TEJA RAYAVARAPU <39586886+rvteja24@users.noreply.github.com>
2019-04-23 18:13:37 +0530

    Update app-routing.module.ts

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './core/home/home.component';
-
+import { ContactComponent} from './core/contact/contact.component';
+import { AboutComponent} from './core/about/about.component';
+import { ServicesOfferedComponent} from './core/services-offered/services-offered.component';
 const routes: Routes = [
-  {
-    path: '',
-    loadChildren: './core/core.module#CoreModule'
-  },
+
+  { path: 'home', component: HomeComponent, pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {path: 'contactUs', component: ContactComponent, pathMatch: 'full' },
+  {path: 'aboutUs', component: AboutComponent, pathMatch: 'full' },
+  {path: 'services', component: ServicesOfferedComponent, pathMatch: 'full' },
 ];
 
 @NgModule({
