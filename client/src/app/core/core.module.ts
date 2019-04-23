a97687dbc5a1c810ccf718cf20054561ea2f99cc
Eeshwar Ankathi <48521820+eeshwarankathi@users.noreply.github.com>
2019-04-23 13:11:25 +0530

    Imported Routing Module and Organised Imports

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -1,19 +1,21 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from './home/home.component';
+import { NgModule } from '@angular/core';
+import { NguCarouselModule } from '@ngu/carousel';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { CoreRoutingModule } from './core-routing.module';
+import { HomeComponent } from './home/home.component';
 import { ServicesOfferedComponent } from './services-offered/services-offered.component';
-import { NguCarouselModule } from '@ngu/carousel';
 
 @NgModule({
   declarations: [HomeComponent, AboutComponent, ContactComponent, ServicesOfferedComponent],
   imports: [
     CommonModule,
-    NguCarouselModule
+    NguCarouselModule,
+    CoreRoutingModule
   ],
   exports: [
-    HomeComponent, AboutComponent, ContactComponent, ServicesOfferedComponent
+    // HomeComponent, AboutComponent, ContactComponent, ServicesOfferedComponent
   ]
 })
 export class CoreModule { }
