72f34260b0b7c87848c28db2deee1a314b47c125
Eeshwar Ankathi <48521820+eeshwarankathi@users.noreply.github.com>
2019-04-23 13:12:15 +0530

    Created Core-Routing Module

---
+++ client/src/app/core/core-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { ServicesOfferedComponent } from './services-offered/services-offered.component';
+
+const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'about-us', component: AboutComponent },
+    { path: 'contact-us', component: ContactComponent },
+    { path: 'services', component: ServicesOfferedComponent },
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class CoreRoutingModule { }
