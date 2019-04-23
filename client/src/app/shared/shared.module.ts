a0a4fcd0935b0a577387cd1953bc34ce73407478
VIJAYA TEJA RAYAVARAPU <39586886+rvteja24@users.noreply.github.com>
2019-04-23 17:38:07 +0530

    Update shared.module.ts
    added http module for contact info post request

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,15 +1,17 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { FooterComponent } from './footer/footer.component';
+import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouterModule} from '@angular/router';
+import {Http} from '@angular/http';
+import {HttpModule} from '@angular/http';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
+  declarations: [HeaderComponent, FooterComponent ],
   imports: [
-    CommonModule,
-    RouterModule
+    CommonModule, BrowserModule, RouterModule, HttpModule
   ],
-  exports: [HeaderComponent, FooterComponent]
+  exports: [HeaderComponent, FooterComponent],
 })
 export class SharedModule { }
