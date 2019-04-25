f82d1f8cb4b061dfec66a75328d0c4b9c4c11636
Eeshwar Ankathi <48521820+eeshwarankathi@users.noreply.github.com>
2019-04-25 17:20:36 +0530

    use lazy loading

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
-import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -12,8 +11,7 @@ import { CoreModule } from './core/core.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule,
-	CoreModule
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
