e20fa26f5c4357cbdaadbdd1ba15495ffedca575
Eeshwar Ankathi <48521820+eeshwarankathi@users.noreply.github.com>
2019-04-25 17:19:43 +0530

    updated app routing to use lazy loading

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './core/home/home.component';
-import { ContactComponent} from './core/contact/contact.component';
-import { AboutComponent} from './core/about/about.component';
-import { ServicesOfferedComponent} from './core/services-offered/services-offered.component';
-const routes: Routes = [
+import { RouterModule, Routes } from '@angular/router';
 
-  { path: 'home', component: HomeComponent, pathMatch: 'full' },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  {path: 'contactUs', component: ContactComponent, pathMatch: 'full' },
-  {path: 'aboutUs', component: AboutComponent, pathMatch: 'full' },
-  {path: 'services', component: ServicesOfferedComponent, pathMatch: 'full' },
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: './core/core.module#CoreModule'
+  },
 ];
 
 @NgModule({
